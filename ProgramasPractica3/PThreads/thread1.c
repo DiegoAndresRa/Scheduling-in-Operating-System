3a46f0f2604ef6e0d56e6abb7e9872c953660a9f
Diego Andres Ramirez Garcia <diego.unam.15@gmail.com>
2023-04-01 20:11:42 -0600

    modified:bash code

--- ProgramasPractica3/PThreads/thread1.c
+++ ProgramasPractica3/PThreads/thread1.c
@@ -1,29 +1,43 @@
 #include <pthread.h>
- #include <stdio.h>
- #define NUM_THREADS     5
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define NUM_THREADS 5
 
- void *PrintHello(void *threadid)
- {
-    long tid;
-    tid = (long)threadid;
-    printf("Hello World! It's me, thread #%ld!\n", tid);
-    pthread_exit(NULL);
- }
+void *PrintHello(void *threadid)
+{
+   long tid;
+   tid = (long)threadid;
+   printf("Hello World! It's me, thread #%ld!\n", tid);
+   pthread_exit(NULL);
+}
 
- int main (int argc, char *argv[])
- {
-    pthread_t threads[NUM_THREADS];
-    int rc;
-    long t;
-    for(t=0; t<NUM_THREADS; t++){
-       printf("In main: creating thread %ld\n", t);
-       rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
-       if (rc){
-          printf("ERROR; return code from pthread_create() is %d\n", rc);
-          exit(-1);
-       }
-    }
+int main(int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for (t = 0; t < NUM_THREADS; t++)
+   {
+      printf("In main: creating thread %ld\n", t);
+      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      if (rc)
+      {
+         printf("ERROR; return code from pthread_create() is %d\n", rc);
+         exit(-1);
+      }
+   }
+   /* Espera a que todos los hilos terminen */
+   for (t = 0; t < NUM_THREADS; t++)
+   {
+      pthread_join(threads[t], NULL);
+   }
 
-    /* Last thing that main() should do */
-    pthread_exit(NULL);
- }
+   /* Muestra la información de los hilos */
+   char command[50];
+   sprintf(command, "ps -T -p %d", getpid());
+   system(command);
+
+   /* Last thing that main() should do */
+   pthread_exit(NULL);
+}
